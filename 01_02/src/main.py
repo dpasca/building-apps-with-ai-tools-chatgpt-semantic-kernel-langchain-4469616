4d77ca7101be818ca151bf66e8605049cfbe1992
Davide Pasca <dpasca@gmail.com>
2023-12-02 05:26:11 +0900

    Getting "hello world" in Chinese

--- 01_02/src/main.py
+++ 01_02/src/main.py
@@ -1,13 +1,16 @@
 import os
 import openai
 from dotenv import load_dotenv
+import json
+
+
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": ""},
+        {"role": "system", "content": "Respond in Chinese"},
         {"role": "user", "content": "Say 'Hello world'"}
     ],
     temperature=0.7,
@@ -15,4 +18,5 @@ response = openai.ChatCompletion.create(
 )
 
 response_message = response["choices"][0]["message"]
-print(response_message)
+txt = json.dumps(response_message, indent=4, ensure_ascii=False)
+print(txt)
